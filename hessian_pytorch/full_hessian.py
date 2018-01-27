91cb4e1878fca0ed90504da2e32fc0f78067f0e1
Mario Geiger <geiger.mario@gmail.com>
2018-01-28 00:31:28 +0100

    rename argument

--- hessian_pytorch/full_hessian.py
+++ hessian_pytorch/full_hessian.py
@@ -5,25 +5,25 @@ Computes the Hessian
 import torch
 
 
-def hessian(output, parameters):
+def hessian(output, inputs):
     '''
-    Compute the Hessian of `output` with respect to `parameters`
+    Compute the Hessian of `output` with respect to `inputs`
     '''
-    if parameters[0].is_cuda:
+    if inputs[0].is_cuda:
         Tensor = torch.cuda.FloatTensor
     else:
         Tensor = torch.FloatTensor
 
-    n = sum(p.numel() for p in parameters)
+    n = sum(p.numel() for p in inputs)
     hess = Tensor(n, n).fill_(0)
 
     ai = 0
-    for i, param in enumerate(parameters):
+    for i, param in enumerate(inputs):
         grad = torch.autograd.grad(output, param, create_graph=True)
         grad = grad[0].contiguous().view(-1)
 
         for j in range(param.numel()):
-            row = torch.autograd.grad(grad[j], parameters[i:], retain_graph=True)
+            row = torch.autograd.grad(grad[j], inputs[i:], retain_graph=True)
             row = torch.cat([x.data.contiguous().view(-1) for x in row])[j:]
 
             hess[ai, ai:] += row
